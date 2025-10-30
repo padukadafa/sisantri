b21f96ad9be2da7a57bb00a79a01c496cd9ccd1f
padukadafa <padukadafa@gmail.com>
2025-10-30 11:15:54 +0700

    feat: add system chrome orientation settings to main application

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -39,6 +40,10 @@ void main() async {
       await NotificationService.initialize();
     } catch (e) {}
   } catch (e) {}
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
   runApp(const ProviderScope(child: SiSantriApp()));
 
